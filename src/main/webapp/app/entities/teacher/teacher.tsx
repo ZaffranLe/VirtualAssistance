5b1d973d592b24a72f773504db655e98fbcbeb66
unknown <biprono12@gmail.com>
2019-03-29 17:00:56 +0700

    Giao diện ds giáo viên + phân quyền ẩn hiện chức năng chỉnh sửa + xoá

--- src/main/webapp/app/entities/teacher/teacher.tsx
+++ src/main/webapp/app/entities/teacher/teacher.tsx
@@ -5,30 +5,38 @@ import { Button, Col, Row, Table } from 'reactstrap';
 // tslint:disable-next-line:no-unused-variable
 import { Translate, ICrudGetAllAction, TextFormat } from 'react-jhipster';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
+import { getSession } from 'app/shared/reducers/authentication';
 import { IRootState } from 'app/shared/reducers';
 import { getEntities } from './teacher.reducer';
 import { ITeacher } from 'app/shared/model/teacher.model';
 // tslint:disable-next-line:no-unused-variable
-import { APP_DATE_FORMAT, APP_LOCAL_DATE_FORMAT } from 'app/config/constants';
+import { hasAnyAuthority } from 'app/shared/auth/private-route';
+import { AUTHORITIES } from 'app/config/constants';
 
 export interface ITeacherProps extends StateProps, DispatchProps, RouteComponentProps<{ url: string }> {}
 
 export class Teacher extends React.Component<ITeacherProps> {
   componentDidMount() {
     this.props.getEntities();
+    this.props.getSession();
   }
 
   render() {
     const { teacherList, match } = this.props;
+    const { account } = this.props;
+    const { isAdmin } = this.props;
     return (
       <div>
         <h2 id="teacher-heading">
           <Translate contentKey="virtualAssistantApp.teacher.home.title">Teachers</Translate>
-          <Link to={`${match.url}/new`} className="btn btn-primary float-right jh-create-entity" id="jh-create-entity">
-            <FontAwesomeIcon icon="plus" />&nbsp;
-            <Translate contentKey="virtualAssistantApp.teacher.home.createLabel">Create new Teacher</Translate>
-          </Link>
+          {teacherList.map(
+            (teacher, i) =>
+              teacher.user.id === account.id && (
+                <Button className="float-right" tag={Link} to={`${match.url}/${teacher.id}/edit`} color="primary">
+                  <FontAwesomeIcon icon="pencil-alt" /> <span className="d-none d-md-inline">Chỉnh sửa thông tin giáo viên của bạn</span>
+                </Button>
+              )
+          )}
         </h2>
         <div className="table-responsive">
           <Table responsive>
@@ -38,40 +46,16 @@ export class Teacher extends React.Component<ITeacherProps> {
                   <Translate contentKey="global.field.id">ID</Translate>
                 </th>
                 <th>
-                  <Translate contentKey="virtualAssistantApp.teacher.identityNumber">Identity Number</Translate>
-                </th>
-                <th>
                   <Translate contentKey="virtualAssistantApp.teacher.fullName">Full Name</Translate>
                 </th>
                 <th>
                   <Translate contentKey="virtualAssistantApp.teacher.phone">Phone</Translate>
                 </th>
                 <th>
-                  <Translate contentKey="virtualAssistantApp.teacher.doB">Do B</Translate>
-                </th>
-                <th>
-                  <Translate contentKey="virtualAssistantApp.teacher.address">Address</Translate>
-                </th>
-                <th>
                   <Translate contentKey="virtualAssistantApp.teacher.email">Email</Translate>
                 </th>
                 <th>
-                  <Translate contentKey="virtualAssistantApp.teacher.dataStorage">Data Storage</Translate>
-                </th>
-                <th>
-                  <Translate contentKey="virtualAssistantApp.teacher.usedStorage">Used Storage</Translate>
-                </th>
-                <th>
                   <Translate contentKey="virtualAssistantApp.teacher.level">Level</Translate>
-                </th>
-                <th>
-                  <Translate contentKey="virtualAssistantApp.teacher.status">Status</Translate>
-                </th>
-                <th>
-                  <Translate contentKey="virtualAssistantApp.teacher.avatar">Avatar</Translate>
-                </th>
-                <th>
-                  <Translate contentKey="virtualAssistantApp.teacher.user">User</Translate>
                 </th>
                 <th />
               </tr>
@@ -84,24 +68,12 @@ export class Teacher extends React.Component<ITeacherProps> {
                       {teacher.id}
                     </Button>
                   </td>
-                  <td>{teacher.identityNumber}</td>
                   <td>{teacher.fullName}</td>
                   <td>{teacher.phone}</td>
-                  <td>
-                    <TextFormat type="date" value={teacher.doB} format={APP_DATE_FORMAT} />
-                  </td>
-                  <td>{teacher.address}</td>
                   <td>{teacher.email}</td>
-                  <td>{teacher.dataStorage}</td>
-                  <td>{teacher.usedStorage}</td>
                   <td>
                     <Translate contentKey={`virtualAssistantApp.TeacherLevel.${teacher.level}`} />
                   </td>
-                  <td>
-                    <Translate contentKey={`virtualAssistantApp.Status.${teacher.status}`} />
-                  </td>
-                  <td>{teacher.avatar}</td>
-                  <td>{teacher.user ? teacher.user.id : ''}</td>
                   <td className="text-right">
                     <div className="btn-group flex-btn-group-container">
                       <Button tag={Link} to={`${match.url}/${teacher.id}`} color="info" size="sm">
@@ -110,18 +82,22 @@ export class Teacher extends React.Component<ITeacherProps> {
                           <Translate contentKey="entity.action.view">View</Translate>
                         </span>
                       </Button>
-                      <Button tag={Link} to={`${match.url}/${teacher.id}/edit`} color="primary" size="sm">
-                        <FontAwesomeIcon icon="pencil-alt" />{' '}
-                        <span className="d-none d-md-inline">
-                          <Translate contentKey="entity.action.edit">Edit</Translate>
-                        </span>
-                      </Button>
-                      <Button tag={Link} to={`${match.url}/${teacher.id}/delete`} color="danger" size="sm">
-                        <FontAwesomeIcon icon="trash" />{' '}
-                        <span className="d-none d-md-inline">
-                          <Translate contentKey="entity.action.delete">Delete</Translate>
-                        </span>
-                      </Button>
+                      {isAdmin && (
+                        <div>
+                          <Button tag={Link} to={`${match.url}/${teacher.id}/edit`} color="primary" size="sm">
+                            <FontAwesomeIcon icon="pencil-alt" />{' '}
+                            <span className="d-none d-md-inline">
+                              <Translate contentKey="entity.action.edit">Edit</Translate>
+                            </span>
+                          </Button>
+                          <Button tag={Link} to={`${match.url}/${teacher.id}/delete`} color="danger" size="sm">
+                            <FontAwesomeIcon icon="trash" />{' '}
+                            <span className="d-none d-md-inline">
+                              <Translate contentKey="entity.action.delete">Delete</Translate>
+                            </span>
+                          </Button>{' '}
+                        </div>
+                      )}
                     </div>
                   </td>
                 </tr>
@@ -134,12 +110,15 @@ export class Teacher extends React.Component<ITeacherProps> {
   }
 }
 
-const mapStateToProps = ({ teacher }: IRootState) => ({
-  teacherList: teacher.entities
+const mapStateToProps = ({ authentication, teacher }: IRootState) => ({
+  teacherList: teacher.entities,
+  isAdmin: hasAnyAuthority(authentication.account.authorities, [AUTHORITIES.ADMIN]),
+  account: authentication.account
 });
 
 const mapDispatchToProps = {
-  getEntities
+  getEntities,
+  getSession
 };
 
 type StateProps = ReturnType<typeof mapStateToProps>;
