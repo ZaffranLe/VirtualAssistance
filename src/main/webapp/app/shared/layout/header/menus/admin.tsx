a5f80a2bb4d925da45750053bc0ca683da6236be
Zaffran <biprono12@gmail.com>
2019-03-30 21:18:33 +0700

    Front-end

--- src/main/webapp/app/shared/layout/header/menus/admin.tsx
+++ src/main/webapp/app/shared/layout/header/menus/admin.tsx
@@ -36,7 +36,7 @@ const swaggerItem = (
 );
 
 export const AdminMenu = ({ showSwagger }) => (
-  <NavDropdown icon="user-plus" name={translate('global.menu.admin.main')} style={{ width: '140%' }} id="admin-menu">
+  <NavDropdown icon="user-plus" name={translate('global.menu.admin.main')} id="admin-menu">
     {adminMenuItems}
     {showSwagger && swaggerItem}
   </NavDropdown>
